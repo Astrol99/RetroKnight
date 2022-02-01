bade6cea69df70c3e2ab094e64031c9d0cc351e7
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2022-01-31 20:43:47 -0600

    Added basic main menu

---
+++ MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	$VBoxContainer/Enter.grab_focus()
+
+
+func _on_Enter_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
