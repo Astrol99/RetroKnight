5b455a3bcb29d46b0efe3439da74e37dbfbfcc07
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2022-02-05 19:20:55 -0600

    Added main character knight sprites

--- Player.gd
+++ Player.gd
@@ -1,40 +1,56 @@
 extends KinematicBody2D
 
+onready var _animated_sprite = $AnimatedSprite
+
 export var acceleration : float = 512.0
 export var max_speed : float = 64.0
 export var friction : float = 0.25
-export var air_resistance : float = 0.02
 export var gravity : float = 200.0
 export var jump_force : float = 128.0
 
 var motion : Vector2 = Vector2.ZERO
 
-#onready const sprite = $Sprite
-
 func _physics_process(delta):
-	# Horizontal movement
+	# Get input of left and right
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	if Input.is_action_just_pressed("player_attack"):
+		set_anim("attack")
 	
+	# Horizontal movement
 	if x_input != 0:
 		motion.x += x_input * acceleration * delta
 		motion.x = clamp(motion.x, -max_speed, max_speed)
-		#sprite.flip_h = x_input < 0
+		_animated_sprite.flip_h = x_input < 0
 	
 	# Gravity
 	motion.y += gravity * delta
 	
-	# Jump
 	if is_on_floor():
-		if x_input == 0:
+		# Slide friction
+		if x_input == 0:	
 			motion.x = lerp(motion.x, 0, friction)
 		
+		# Jump
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -jump_force
-	else:
-		if Input.is_action_just_pressed("ui_up") and motion.y < -jump_force/2:
-			motion.y = -jump_force/2
-		
-		if x_input == 0:
-			motion.x = lerp(motion.x, 0, air_resistance)
 	
 	motion = move_and_slide(motion, Vector2.UP)
+	
+	# Main animation
+	if is_on_floor():
+		if x_input == 0:
+			set_anim("idle")
+		else:
+			set_anim("run")
+	else:
+		if motion.y < 0:
+			set_anim("jump")
+		else:
+			set_anim("fall")
+
+func set_anim(anim):
+	if _animated_sprite.animation != anim:
+		_animated_sprite.play(anim)
+	else:
+		return
