fa624ab5daef10d2f33511d82aa9357db944eb98
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2022-02-06 22:05:37 -0600

    Added attack animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 onready var _sprite = $Sprite
-onready var _animation_player = $AnimationPlayer
-onready var _animation_state = $AnimationTree.get("parameters/playback")
+onready var _animation_tree = $AnimationTree
+onready var _animation_state = _animation_tree.get("parameters/playback")
 
 export var ACCELERATION : float = 500.0
 export var MAX_SPEED : float = 150.0
@@ -10,9 +10,25 @@ export var FRICTION : float = 0.25
 export var GRAVITY : float = 400.0
 export var JUMP_FORCE : float = 200.0
 
+enum {
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
 var velocity : Vector2 = Vector2.ZERO
 
-func _physics_process(delta):		
+func _ready():
+	_animation_tree.active = true
+
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):	
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	# Horizontal movement
@@ -23,21 +39,27 @@ func _physics_process(delta):
 		
 		if is_on_floor():
 			_animation_state.travel("run")
-	else:
-		if is_on_floor():
-			_animation_state.travel("idle")
+			
+	elif x_input == 0 and is_on_floor():
+		velocity.x = lerp(velocity.x, 0, FRICTION)
+		_animation_state.travel("idle")
 
 	if is_on_floor():
-		# Friction
-		if x_input == 0:	
-			velocity.x = lerp(velocity.x, 0, FRICTION)
-		
 		# Jump
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = -JUMP_FORCE
 			_animation_state.travel("jump")
 
 	velocity.y += GRAVITY * delta
-	
-	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if Input.is_action_just_pressed("player_attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	_animation_state.travel("attack")
+
+func attack_animation_finished():
+	state = MOVE
+	
