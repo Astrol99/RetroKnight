b0c60e1b738865cf875bd726c4c61a235522f7e9
David <dngames2006@gmail.com>
2022-02-26 22:02:20 -0600

    Added basic enemy AI

--- scripts/Flying Eye.gd
+++ scripts/Flying Eye.gd
@@ -1,16 +1,62 @@
 extends KinematicBody2D
 
-onready var stats = $Stats
+onready var _stats = $Stats
+onready var _animated_sprite = $AnimatedSprite
+onready var _player_detection_zone = $PlayerDetectionZone
 
+export var ACCELERATION = 50
+export var MAX_SPEED = 500
+export var FRICTION = 200
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var state = CHASE
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+			
+		CHASE:
+			var player = _player_detection_zone.player
+			if player:
+				var direction = position.direction_to(player.global_position)
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+	
+			_animated_sprite.flip_h = velocity.x < 0
+	
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if _player_detection_zone.player:
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
-	stats.health -= area.damage
 	knockback = area.knockback_vector * 100
+	
+	_animated_sprite.play("takehit")
+	yield(_animated_sprite, "animation_finished")
+	_animated_sprite.play("flight")
+	
+	_stats.health -= area.damage
 
 func _on_Stats_no_health():
+	_animated_sprite.play("death")
+	yield(_animated_sprite, "animation_finished")
+	
 	queue_free()
---
+++ scripts/PlayerDetectionZone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var player = null  
+
+func _ready():
+	pass
+
+func _on_PlayerDetectionZone_body_entered(body):
+	player = body
+
+func _on_PlayerDetectionZone_body_exited(body):
+	player = null
